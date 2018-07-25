2a8c8554dc0eb1765e3ba4c8a316fcd4f1be4945
cauvong9696 <cauvong9696@gmail.com>
2018-07-25 14:45:42 +0700

    Show shoes page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require effects
 //= require java
-//= require lightbox
 //= require prototype
 //= require scriptaculous
+//= require effects
+//= require lightbox
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
-    get "/detail", to: "static_pages#detail"
-    get "/shoes", to:"static_pages#shoes"
     get "/cart", to:"carts#view_cart"
     get "/payment", to:"carts#payment"
     get "/bill", to:"carts#bill"
